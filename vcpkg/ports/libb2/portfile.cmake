483994dd0041e8a3ebf3147cadfbcbe07ca6db02
Michael Schmuki <michael@opengis.ch>
2025-03-31 17:45:55 +0200

    Adapt libb2 port to fix failing build on iOS

--- vcpkg/ports/libb2/portfile.cmake
+++ vcpkg/ports/libb2/portfile.cmake
@@ -1,22 +1,27 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO BLAKE2/libb2
-    REF 2c5142f12a2cd52f3ee0a43e50a3a76f75badf85
-    SHA512 cf29cf9391ae37a978eb6618de6f856f3defa622b8f56c2d5a519ab34fd5e4d91f3bb868601a44e9c9164a2992e80dde188ccc4d1605dffbdf93687336226f8d
+    REF 643decfbf8ae600c3387686754d74c84144950d1
+    SHA512 4727633659900d7726a7c64dddd88be1952265b0857ed5c8e69e0e03ad0d702dd3e6ff948fa357957be0ebdfcd8f523d4e60bd3c141229bb43c5faaf933bdc6b
     HEAD_REF master
 )
 
-set(OPTIONS)
-if(CMAKE_HOST_WIN32)
-    set(OPTIONS --disable-native) # requires cpuid
+vcpkg_list(SET options)
+
+#if(CMAKE_HOST_WIN32)
+    vcpkg_list(APPEND options "--disable-native")
+#endif()
+
+if(VCPKG_TARGET_IS_IOS)
+    # see https://github.com/microsoft/vcpkg/pull/17912#issuecomment-840514179
+    vcpkg_list(APPEND options "ax_cv_check_cflags___O3=no")
 endif()
 
 vcpkg_configure_make(
     AUTOCONFIG
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
-        ax_cv_check_cflags___O3=no # see https://github.com/microsoft/vcpkg/pull/17912#issuecomment-840514179
-        ${OPTIONS}
+        ${options}
 )
 vcpkg_install_make()
 vcpkg_fixup_pkgconfig()

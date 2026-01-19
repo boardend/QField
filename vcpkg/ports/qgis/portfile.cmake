eb2a3faefdea112f4a39688ccebc9b29993a213c
Michael Schmuki <mschmuki@ethz.ch>
2026-01-19 13:37:10 +0100

    Cleanup

--- vcpkg/ports/qgis/portfile.cmake
+++ vcpkg/ports/qgis/portfile.cmake
@@ -17,11 +17,10 @@ vcpkg_from_github(
         include-qthread.patch
         processing.patch # Needed to avoid link issue with tinygltf (ATM embedded into QGIS) and _GEOSQueryCallback defined multiple times
         locatorcontext.patch # Remove when upgrading to QGIS 3.42
-        rectangle.patch # Remove when upgrading to QGIS 3.42
+        rectangle.patch # Remove when upgrading to QGIS 3.42  
         oauth-headers.patch
         andorid_extras_qt6.patch
         ios-arm64.patch
-        # debug-constructors.patch # Add debug prints to constructors/destructors for debugging
 )
 
 file(REMOVE ${SOURCE_PATH}/cmake/FindGDAL.cmake)

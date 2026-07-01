c08f7a76826a37be058ed4ab393cb712d5b67507
Michael Schmuki <michael@schmuki.io>
2026-07-01 23:18:06 +0200

    Windows debug build: qgis-cmake-wrapper spatialindex/qca-ossl + CMake config
    - qgis-cmake-wrapper: select spatialindex-64/32 by arch, use -64d/-32d debug libs,
      resolve qca-ossl with debug/release variants
    - CMakeLists: force Debug;Release configuration types; add /DEBUG link option
    - vcpkg.json: lerc dependency; build host qgis only on non-Windows

--- cmake/qgis-cmake-wrapper.cmake
+++ cmake/qgis-cmake-wrapper.cmake
@@ -121,7 +121,16 @@ if(TRUE) # Should possibly have a "static only" check
   endif()
   # End Terrible hack
 
-  find_library(Qca-ossl_LIBRARIES NAMES qca-ossl PATH_SUFFIXES Qca/crypto)
+  # Find qca-ossl with proper debug/release variants
+  find_library(Qca-ossl_LIBRARIES_RELEASE NAMES qca-ossl PATH_SUFFIXES Qca/crypto)
+  find_library(Qca-ossl_LIBRARIES_DEBUG NAMES qca-ossld PATH_SUFFIXES Qca/crypto)
+  if(Qca-ossl_LIBRARIES_DEBUG AND Qca-ossl_LIBRARIES_RELEASE)
+    set(Qca-ossl_LIBRARIES optimized ${Qca-ossl_LIBRARIES_RELEASE} debug ${Qca-ossl_LIBRARIES_DEBUG})
+  elseif(Qca-ossl_LIBRARIES_RELEASE)
+    set(Qca-ossl_LIBRARIES ${Qca-ossl_LIBRARIES_RELEASE})
+  elseif(Qca-ossl_LIBRARIES_DEBUG)
+    set(Qca-ossl_LIBRARIES ${Qca-ossl_LIBRARIES_DEBUG})
+  endif()
   target_link_libraries(QGIS::Core INTERFACE ${Qca-ossl_LIBRARIES})
 
   _qgis_core_add_dependency(GDAL::GDAL GDAL)
@@ -133,7 +142,11 @@ if(TRUE) # Should possibly have a "static only" check
   _qgis_core_add_dependency(libzip::zip libzip)
   _qgis_core_add_dependency(ZLIB::ZLIB ZLIB)
   if(MSVC)
-    _find_and_link_library(spatialindex-64 QGIS::Core)
+   if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+      _find_and_link_library(spatialindex-64d QGIS::Core)
+   else()
+      _find_and_link_library(spatialindex-32d QGIS::Core)
+   endif()
   else()
     _find_and_link_library(spatialindex QGIS::Core)
   endif()
